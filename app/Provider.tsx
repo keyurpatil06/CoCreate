15e7d2b6f769b23464e601a7c1c8d0f0137f0629
keyurpatil06 <keyurpatil3000@gmail.com>
2024-07-22 20:11:09 +0530

    implement liveblocks and collaborative editor room

---
+++ app/Provider.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Loader from '@/components/Loader';
+import { getClerkUser } from '@/lib/actions/user.actions';
+import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense'
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <LiveblocksProvider
+      authEndpoint='/api/liveblocks-auth'
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUser({ userIds });
+
+        return users;
+      }}
+    >
+      <ClientSideSuspense fallback={<Loader />}>
+        {children}
+      </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default Provider
